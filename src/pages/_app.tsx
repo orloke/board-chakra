85eea28cb2f0e63a165426c8276b4a013f83aeae
Junior Dering <juniordering@hotmail.com>
2022-09-20 08:42:48 -0400

    feat: install react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import SidebarDrawerProvider from '../contexts/SidebarDrawer';
@@ -7,12 +8,17 @@ import { makeServer } from '../services/mirage';
 if (process.env.NODE_ENV === 'development') {
   makeServer({ environment: 'development' });
 }
+
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
+      <QueryClientProvider client={queryClient}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,34 +1,17 @@
-import {
-  Box,
-  Button,
-  Checkbox,
-  Flex,
-  Heading,
-  Icon,
-  Table,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr,
-} from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Icon } from '@chakra-ui/react';
 import Link from 'next/link';
-import { useEffect } from 'react';
-import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
+import { RiAddLine } from 'react-icons/ri';
 import Header from '../../components/Header';
-import { Pagination } from '../../components/Pagination';
 import Sidebar from '../../components/Sidebar';
+import RenderUsersList from '../../components/RenderUsersList';
 
 export default function UsersList() {
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => {
-        return response.json();
-      })
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-  }, []);
+  const { data, isLoading, error } = useQuery('userList', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const users = await response.json();
+    return users;
+  });
 
   return (
     <Box>
@@ -52,47 +35,7 @@ export default function UsersList() {
               </Button>
             </Link>
           </Flex>
-          <Table px="6" color="gray.300" colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th pl="0" pr="6">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuarios</Th>
-                <Th>Data de cadastro</Th>
-                <Th width="0" />
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td pl="0" pr="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Júnior Dering</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      juniordering@hotmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>11 de setembro, 2022</Td>
-                <Td pr="0">
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    bg="pink.200"
-                    color="black"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
-                  >
-                    Adicionar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          <RenderUsersList error={error} isLoading={isLoading} data={data} />
         </Box>
       </Flex>
     </Box>
